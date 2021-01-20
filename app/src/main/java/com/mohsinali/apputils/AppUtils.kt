d442e527454ea8dd1f8a8629c4338905818d221a
MohsinaliEMed <mohsinali.emedstore@gmail.com>
2021-01-20 16:33:59 +0530

    create object

--- app/src/main/java/com/mohsinali/apputils/AppUtils.kt
+++ app/src/main/java/com/mohsinali/apputils/AppUtils.kt
@@ -4,7 +4,7 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.TextView
 
-object AppUtils {
+object AppUtilsSecond {
     fun isEmpty(textView: TextView, stringValue: String) {
         textView.text = if (!TextUtils.isEmpty(stringValue)) stringValue else ""
         textView.visibility = if (!TextUtils.isEmpty(stringValue)) View.VISIBLE else View.GONE
