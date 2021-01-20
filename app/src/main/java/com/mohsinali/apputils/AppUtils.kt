a3f1a173b217daa1bd52cb1d8737bbfbd2939e9a
MohsinaliEMed <mohsinali.emedstore@gmail.com>
2021-01-20 12:57:57 +0530

    Initial commit

---
+++ app/src/main/java/com/mohsinali/apputils/AppUtils.kt
@@ -0,0 +1,12 @@
+package com.mohsinali.apputils
+
+import android.text.TextUtils
+import android.view.View
+import android.widget.TextView
+
+class AppUtils {
+    fun isEmpty(textView: TextView, stringValue: String) {
+        textView.text = if (!TextUtils.isEmpty(stringValue)) stringValue else ""
+        textView.visibility = if (!TextUtils.isEmpty(stringValue)) View.VISIBLE else View.GONE
+    }
+}
